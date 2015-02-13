0a5865e581d8d935c9d1dbfa2774ae81f2f507c1
cristianmadridd <cristianmadridd@gmail.com>
2015-02-13 17:16:36 -0200

    Commit basic configuration for views

---
+++ EnglishHour/EHProfile.h
@@ -0,0 +1,13 @@
+//
+//  EHProfile.h
+//  EnglishHour
+//
+//  Created by Cristian Madrid on 2/13/15.
+//  Copyright (c) 2015 Cristian Madrid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EHProfile : NSObject
+
+@end
---
+++ EnglishHour/EHProfile.m
@@ -0,0 +1,13 @@
+//
+//  EHProfile.m
+//  EnglishHour
+//
+//  Created by Cristian Madrid on 2/13/15.
+//  Copyright (c) 2015 Cristian Madrid. All rights reserved.
+//
+
+#import "EHProfile.h"
+
+@implementation EHProfile
+
+@end

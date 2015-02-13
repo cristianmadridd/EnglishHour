130103e29b619b3d7bb6af8cbfd38c8dc8afcec6
cristianmadridd <cristianmadridd@gmail.com>
2015-02-13 13:36:54 -0200

    First commit

---
+++ EnglishHour/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EnglishHour
+//
+//  Created by Cristian Madrid on 2/13/15.
+//  Copyright (c) 2015 Cristian Madrid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EnglishHour/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnglishHour
+//
+//  Created by Cristian Madrid on 2/13/15.
+//  Copyright (c) 2015 Cristian Madrid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
